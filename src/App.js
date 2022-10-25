717122ab07ad0ca1cd87c174456b26d02451146a
battulabharath27 <58397989+battulabharath27@users.noreply.github.com>
2022-10-24 21:30:12 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { Navbar } from './components/Navbar';
+import { TodoRows } from './components/TodoRows';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+         userName: 'Joey',
+         todoItems: [
+           {action: 'Get $100k job', done: true },
+           {action: 'Get Permanent Residency', done: false},
+           {action: 'Get into one of FAANG Companies', done: false},
+           {action: 'Go to Home', done: false},
+         ],
+         newToDo: '',  
+    };
+  }
+
+  toggleDone = (todo) => {
+    this.setState({
+      todoItems: this.state.todoItems.map((item) =>
+           item.action === todo.action ? {...item, done: !item.done} : item 
+      ),
+    })
+  }
+
+ todoRows = () => 
+    this.state.todoItems.map((item) => (
+     <TodoRows 
+       key={item.action}
+       item = {item}
+       callback = {this.toggleDone}
+      />
+    ));
+
+  updateValue = (event) => {
+    this.setState({newToDo: event.target.value});
+  }; 
+
+  newToDo = () => {
+    this.setState({
+      todoItems: [
+        ...this.state.todoItems,
+        {action: this.state.newToDo, done: false},
+      ]
+    })
+  }
+
+  render = () => (
+    <div className="container">
+      <div className="row">
+       <Navbar name={this.state.userName} />
+        <div className="col-12">
+          <input 
+          className="form-control" 
+          value={this.state.newToDo}
+          onChange={this.updateValue}
+          />
+          <button className="btn btn-primary" onClick={this.newToDo}>
+            Add
+          </button>
+        </div>
+       
+        <div className="col-12">
+          <table className="table">
+             <thead>
+               <tr>
+                 <th>Task</th>
+                 <th>Complete</th>
+               </tr>
+             </thead>
+            <tbody>{this.todoRows()}</tbody>
+          </table>
+        </div>
+      </div>
+    </div> 
   );
 }
-
-export default App;
